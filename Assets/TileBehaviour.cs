0d70812c5e9a983f7cd272911c12a61bcd3cdda5
EchoFrost <whitesellbt@gmail.com>
2018-02-23 00:48:16 -0800

    Modified trigger prefab, set board tile fields
    Added trigger box collider to tile prefab, added TileBehaviour script to tile prefab, set directional properties of map tiles

---
+++ Assets/TileBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBehaviour : MonoBehaviour {
+    [SerializeField]
+    public bool TopRight;
+    [SerializeField]
+    public bool TopLeft;
+    [SerializeField]
+    public bool BottomRight;
+    [SerializeField]
+    public bool BottomLeft;
+    [SerializeField]
+    public int GoldOffset;
+    [SerializeField]
+    public bool EnemyTrigger;
+    [SerializeField]
+    public bool TeleportTrigger;
+    [SerializeField]
+    public bool GoalCheckTrigger;
+}
