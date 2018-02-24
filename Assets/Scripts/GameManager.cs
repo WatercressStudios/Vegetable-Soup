e4602a346eca92deac34ba12d6926ac64356351c
EchoFrost <whitesellbt@gmail.com>
2018-02-24 15:44:35 -0800

    Created GameManager class with related GameSequence and GenerateRandomNumber methods

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int _turnCounter = 0;
+    private int _moveRoll;
+
+	void Start ()
+    {
+        GameSequence();
+	}
+
+    public void GameSequence()
+    {
+        _turnCounter++;
+        _moveRoll = GenerateRandomNumber();
+        Debug.Log(_moveRoll);
+    }
+
+    public int GenerateRandomNumber()
+    {
+        return Random.Range(1, 6);
+    }
+}
