a3332eacf7ef4d7f8d52b1f7a8da35e546219f19
EchoFrost <whitesellbt@gmail.com>
2018-02-25 18:16:08 -0800

    Renamed hometile script to TileMovement, allows for simple tile movement
    Renamed HomeTile to TileMovement. Script now pulls in information about the legal movement directions and applies those movements.

---
+++ Assets/Scripts/TileMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMovement : MonoBehaviour
+{
+    public GameObject Tile;
+    void OnTriggerEnter (Collider other)
+    {
+        // <remarks>
+        // I eventually want to remove 'Tile' and get that information from our Collider.
+        // </remarks>
+
+        GameObject Player = other.gameObject;
+
+        if (Tile.GetComponent<TileBehaviour>().TopRight)
+        {
+            Vector3 moveValue = new Vector3(0,0,1.1f);
+            Player.transform.position += moveValue;
+            Debug.Log("You can move to the TopRight");
+        }
+        else if (Tile.GetComponent<TileBehaviour>().TopLeft)
+        {
+            Vector3 moveValue = new Vector3(1.1f, 0, 0);
+            Player.transform.position -= moveValue;
+            Debug.Log("You can move to the TopLeft");
+        }
+        else if (Tile.GetComponent<TileBehaviour>().BottomRight)
+        {
+            Vector3 moveValue = new Vector3(1.1f, 0, 0);
+            Player.transform.position += moveValue;
+            Debug.Log("You can move to the BottomRight");
+        }
+        else if (Tile.GetComponent<TileBehaviour>().BottomLeft)
+        {
+            Vector3 moveValue = new Vector3(0, 0, 1.1f);
+            Player.transform.position -= moveValue;
+            Debug.Log("You can move to the BottomLeft");
+        }
+        else
+        {
+            Debug.Log("You apparently can't move anywhere!");
+        }
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        //Debug.Log("Object is within trigger");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //Debug.Log("Object exited the trigger");
+    }
+}
