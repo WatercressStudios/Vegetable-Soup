57a4fc803bd3e430c1ce920a9c7a66993f5bd04d
EchoFrost <whitesellbt@gmail.com>
2018-02-25 17:04:35 -0800

    Created character prefab, began hometile script

---
+++ Assets/Scripts/HomeTile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeTile : MonoBehaviour
+{
+    void OnTriggerEnter (Collider other)
+    {
+        Debug.Log("Object entered the trigger");
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Object is within trigger");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Object exited the trigger");
+    }
+}
