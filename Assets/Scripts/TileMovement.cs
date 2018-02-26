66fe3fa3fba287cca8fcf53acea1436ba3702052
EchoFrost <whitesellbt@gmail.com>
2018-02-25 18:42:35 -0800

    Committing old scene edits and movementwiat coroutine
    The movementwait coroutine doesn't quite work yet, but I figured I should at least save my progress.

--- Assets/Scripts/TileMovement.cs
+++ Assets/Scripts/TileMovement.cs
@@ -11,31 +11,33 @@ public class TileMovement : MonoBehaviour
         // I eventually want to remove 'Tile' and get that information from our Collider.
         // </remarks>
 
+        StartCoroutine(MovementWait());
+
         GameObject Player = other.gameObject;
 
         if (Tile.GetComponent<TileBehaviour>().TopRight)
         {
+            StartCoroutine(MovementWait());
             Vector3 moveValue = new Vector3(0,0,1.1f);
             Player.transform.position += moveValue;
-            Debug.Log("You can move to the TopRight");
         }
         else if (Tile.GetComponent<TileBehaviour>().TopLeft)
         {
+            StartCoroutine(MovementWait());
             Vector3 moveValue = new Vector3(1.1f, 0, 0);
             Player.transform.position -= moveValue;
-            Debug.Log("You can move to the TopLeft");
         }
         else if (Tile.GetComponent<TileBehaviour>().BottomRight)
         {
+            StartCoroutine(MovementWait());
             Vector3 moveValue = new Vector3(1.1f, 0, 0);
             Player.transform.position += moveValue;
-            Debug.Log("You can move to the BottomRight");
         }
         else if (Tile.GetComponent<TileBehaviour>().BottomLeft)
         {
+            StartCoroutine(MovementWait());
             Vector3 moveValue = new Vector3(0, 0, 1.1f);
             Player.transform.position -= moveValue;
-            Debug.Log("You can move to the BottomLeft");
         }
         else
         {
@@ -52,5 +54,10 @@ public class TileMovement : MonoBehaviour
     void OnTriggerExit(Collider other)
     {
         //Debug.Log("Object exited the trigger");
+    }
+
+    IEnumerator MovementWait()
+    {
+        yield return new WaitForSeconds(1);
     }
 }
