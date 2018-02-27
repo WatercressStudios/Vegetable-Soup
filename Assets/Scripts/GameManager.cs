8846aa8c552c816d72e2647df1ef2b805368f5a4
EchoFrost <whitesellbt@gmail.com>
2018-02-26 22:49:27 -0800

    Character now moves according to roll

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,21 +6,80 @@ public class GameManager : MonoBehaviour
 {
     private int _turnCounter = 0;
     private int _moveRoll;
+    public int frame;
 
-	void Start ()
+    void Start ()
     {
-        GameSequence();
+        StartCoroutine(Example());
 	}
+
+    void Update()
+    {
+        if (frame <= 10)
+        {
+            Debug.Log("Frame: " + frame);
+            frame++;
+        }
+    }
 
     public void GameSequence()
     {
         _turnCounter++;
         _moveRoll = GenerateRandomNumber();
         Debug.Log(_moveRoll);
+
+        for (int movements = 0; movements < _moveRoll; movements++)
+        {
+            Debug.Log("Making a movement!");
+            MoveCharacter();
+        }
+    }
+
+    IEnumerator Example()
+    {
+        yield return new WaitUntil(() => frame >= 10);
+        GameSequence();
     }
 
     public int GenerateRandomNumber()
     {
         return Random.Range(1, 6);
+    }
+
+    public void MoveCharacter()
+    {
+        var Player = GameObject.Find("Character");
+        var Tile = GameObject.Find("Character").GetComponent<TileMovement>().Tile;
+
+        Debug.Log("Tile is: " + Tile.name);
+
+        if (Tile.GetComponent<TileBehaviour>().TopRight)
+        {
+            Debug.Log("Top right");
+            Vector3 moveValue = new Vector3(0, 0, 1.1f);
+            Player.transform.position += moveValue;
+        }
+        else if (Tile.GetComponent<TileBehaviour>().TopLeft)
+        {
+            Debug.Log("Top left");
+            Vector3 moveValue = new Vector3(1.1f, 0, 0);
+            Player.transform.position -= moveValue;
+        }
+        else if (Tile.GetComponent<TileBehaviour>().BottomRight)
+        {
+            Debug.Log("Bottom right");
+            Vector3 moveValue = new Vector3(1.1f, 0, 0);
+            Player.transform.position += moveValue;
+        }
+        else if (Tile.GetComponent<TileBehaviour>().BottomLeft)
+        {
+            Debug.Log("Bottom left");
+            Vector3 moveValue = new Vector3(0, 0, 1.1f);
+            Player.transform.position -= moveValue;
+        }
+        else
+        {
+            Debug.Log("You apparently can't move anywhere!");
+        }
     }
 }
--- Assets/Scripts/TileMovement.cs
+++ Assets/Scripts/TileMovement.cs
@@ -5,59 +5,8 @@ using UnityEngine;
 public class TileMovement : MonoBehaviour
 {
     public GameObject Tile;
-    void OnTriggerEnter (Collider other)
+    void OnCollisionEnter (Collision collision)
     {
-        // <remarks>
-        // I eventually want to remove 'Tile' and get that information from our Collider.
-        // </remarks>
-
-        StartCoroutine(MovementWait());
-
-        GameObject Player = other.gameObject;
-
-        if (Tile.GetComponent<TileBehaviour>().TopRight)
-        {
-            StartCoroutine(MovementWait());
-            Vector3 moveValue = new Vector3(0,0,1.1f);
-            Player.transform.position += moveValue;
-        }
-        else if (Tile.GetComponent<TileBehaviour>().TopLeft)
-        {
-            StartCoroutine(MovementWait());
-            Vector3 moveValue = new Vector3(1.1f, 0, 0);
-            Player.transform.position -= moveValue;
-        }
-        else if (Tile.GetComponent<TileBehaviour>().BottomRight)
-        {
-            StartCoroutine(MovementWait());
-            Vector3 moveValue = new Vector3(1.1f, 0, 0);
-            Player.transform.position += moveValue;
-        }
-        else if (Tile.GetComponent<TileBehaviour>().BottomLeft)
-        {
-            StartCoroutine(MovementWait());
-            Vector3 moveValue = new Vector3(0, 0, 1.1f);
-            Player.transform.position -= moveValue;
-        }
-        else
-        {
-            Debug.Log("You apparently can't move anywhere!");
-        }
-
-    }
-
-    void OnTriggerStay(Collider other)
-    {
-        //Debug.Log("Object is within trigger");
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        //Debug.Log("Object exited the trigger");
-    }
-
-    IEnumerator MovementWait()
-    {
-        yield return new WaitForSeconds(1);
+        Tile = collision.gameObject;
     }
 }
